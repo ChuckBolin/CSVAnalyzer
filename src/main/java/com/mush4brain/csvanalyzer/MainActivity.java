cce375ded554cf32fa6e0bbc2c9e727bbfda6ace
Chuck Bolin <cbolin@sc.rr.com>
2015-07-02 17:35:36 -0400

    Added button to display toast

--- src/main/java/com/mush4brain/csvanalyzer/MainActivity.java
+++ src/main/java/com/mush4brain/csvanalyzer/MainActivity.java
@@ -9,7 +9,10 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.NetworkOnMainThreadException;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,29 +21,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{//} implements View.OnClickListener {
   private String TAG = "MAIN ACTIVITY";
-  //detects swipes
-//  float x1,x2;
-//  float y1, y2;
   final String mURL="https://raw.githubusercontent.com/tillnagel/unfolding/master/data/data/countries-population-density.csv";
-
-
   TextView mTextView;// =
-  //mTextView.setMovementMethod(LinkMovementMethod.getInstance());
-
-
-
-//  //fragment stuff
-//  FragmentTransaction ft = null;
-//  PlaceholderFragment fragMain = new PlaceholderFragment();
-//  PlaceholderFragmentRight fragRight = new PlaceholderFragmentRight();
-//  PlaceholderFragmentLeft fragLeft = new PlaceholderFragmentLeft();
-//
-//  final int FRAGMENT_LEFT = 0;
-//  final int FRAGMENT_MAIN = 1;
-//  final int FRAGMENT_RIGHT = 2;
-//  int position = FRAGMENT_MAIN; //indicates current fragment
+  Button mButtonFindFile;
 
 
   @Override
@@ -49,6 +34,15 @@ public class MainActivity extends Activity {
     setContentView(R.layout.activity_main);
 
     mTextView = (TextView)findViewById(R.id.textView);
+    mButtonFindFile = (Button)findViewById(R.id.buttonFindFile);
+    mButtonFindFile.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View v){
+        //findFileClick();
+        Toast.makeText(getApplicationContext(), "Clicked2", Toast.LENGTH_LONG).show();
+      }
+    });
+
 //    Log.d(TAG, mURL);
 
     //start thread
@@ -68,126 +62,19 @@ public class MainActivity extends Activity {
     actionBar.setTitle("CSV Analyzer");
     actionBar.setSubtitle(R.string.app_version);
     actionBar.setBackgroundDrawable(new ColorDrawable(Color.BLUE));
-
-
-//    //sets main fragment
-//    ft = getFragmentManager().beginTransaction();
-//    ft.replace(R.id.container, fragMain);
-//    ft.commit();
   }
 
-//  public boolean onTouchEvent(MotionEvent touchevent){
-//    switch (touchevent.getAction())               {
-//      // when user first touches the screen we get x and y coordinate
-//      case MotionEvent.ACTION_DOWN:
-//      {
-//        x1 = touchevent.getX();
-//        y1 = touchevent.getY();
-//        break;
-//      }
-//      case MotionEvent.ACTION_UP:
-//      {
-//        x2 = touchevent.getX();
-//        y2 = touchevent.getY();
+//  public void onClick(View v){
+//    Toast.makeText(this,"Clicked2", Toast.LENGTH_LONG).show();
+//    if(v.getId() == R.id.buttonFindFile) {
+//     Toast.makeText(this,"Clicked", Toast.LENGTH_LONG).show();
 //
-//        //if left to right sweep event on screen
-//        if (x1 < x2){
-//          position++;
-//          if(position > 2)
-//            position = 2;
-//
-//          if (position == FRAGMENT_MAIN){
-//            ft = getFragmentManager().beginTransaction();
-//            ft.replace(R.id.container, fragMain);
-//            ft.addToBackStack(null);
-//            ft.commit();
-//          }
-//          else if (position == FRAGMENT_RIGHT){
-//            ft = getFragmentManager().beginTransaction();
-//            ft.replace(R.id.container, fragRight);
-//            ft.addToBackStack(null);
-//            ft.commit();
-//          }
-//        }
-//
-//        // if right to left sweep event on screen
-//        if (x1 > x2){
-//          position--;
-//          if(position < 0)
-//            position = 0;
-//
-//          if (position == FRAGMENT_MAIN){
-//            ft = getFragmentManager().beginTransaction();
-//            ft.replace(R.id.container, fragMain);
-//            ft.addToBackStack(null);
-//            ft.commit();
-//          }
-//          else if (position == FRAGMENT_LEFT){
-//            ft = getFragmentManager().beginTransaction();
-//            ft.replace(R.id.container, fragLeft);
-//            ft.addToBackStack(null);
-//            ft.commit();
-//          }
-//        }
-//        break;
-//      }
-//    }
-//    return false;
-//  }
-
-//  //Main fragment
-//  public static class PlaceholderFragment extends Fragment {
-//
-//    public PlaceholderFragment() {
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//      View rootView = inflater
-//              .inflate(R.layout.fragment_main, container, false);
-//
-//      Toast.makeText(this.getActivity(), "Loading CSV File", Toast.LENGTH_LONG).show();
-//
-//
-//
-//      return rootView;
-//    }
-//
-//
-//
-//  }
-//
-//  //Right fragment
-//  public static class PlaceholderFragmentRight extends Fragment {
-//
-//    public PlaceholderFragmentRight() {
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//      View rootView = inflater
-//              .inflate(R.layout.fragment_right, container, false);
-//      return rootView;
-//    }
-//  }
-//
-//  //Left fragment
-//  public static class PlaceholderFragmentLeft extends Fragment {
-//
-//    public PlaceholderFragmentLeft() {
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//      View rootView = inflater
-//              .inflate(R.layout.fragment_left, container, false);
-//      return rootView;
 //    }
 //  }
 
+//  public void findFileClick(View v){
+//
+//  }
 
   class Task implements Runnable {
 
@@ -217,7 +104,5 @@ public class MainActivity extends Activity {
       }
     }
   }
-
-
 }
 
