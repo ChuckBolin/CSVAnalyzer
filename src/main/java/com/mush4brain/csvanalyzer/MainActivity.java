26dc38267423dbd380dbe73bd55df38ae0e2875a
Chuck Bolin <cbolin@sc.rr.com>
2015-07-02 23:45:08 -0400

    Working with folder paths. Not yet perfect.

--- src/main/java/com/mush4brain/csvanalyzer/MainActivity.java
+++ src/main/java/com/mush4brain/csvanalyzer/MainActivity.java
@@ -66,6 +66,14 @@ public class MainActivity extends Activity{
 
   }
 
+  //download mURL to mTextView
+  public void onButtonClick(View v){
+    mTextView.setText("");
+    Toast.makeText(getApplicationContext(), "Downloading...", Toast.LENGTH_SHORT).show();
+    DownloadAsyncTask task = new DownloadAsyncTask();
+    task.execute();
+  }
+
   //download URL AsyncTask
   private class DownloadAsyncTask extends AsyncTask<String, Void, String> {
 
@@ -103,25 +111,8 @@ public class MainActivity extends Activity{
     }
   }
 
-  //read file AsyncTask
-  private class ReadFileAsyncTask extends AsyncTask<String, Void, String> {
-
-    @Override
-    protected String doInBackground(String... urls) {
-      String response = "";
-
-
-
-      return response;
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-      //Toast.makeText(mContext, "onPostExecute", Toast.LENGTH_LONG).show();
-      mTextView.setText(result);
-    }
-  }
-
+  //Button to find file on device is clicked,
+  //then calls onActivityResults with filepath
   public void onButtonFindClick(View v){
     mTextView.setText("");
     Toast.makeText(getApplicationContext(), "Finding...", Toast.LENGTH_SHORT).show();
@@ -145,48 +136,55 @@ public class MainActivity extends Activity{
       case 0:
         if(resultCode == RESULT_OK){
           Uri uri = data.getData();
-          //try {
-            String fileContents = readTextFile(uri);// getPath(this, uri.getPath());
-            mTextView.setText(fileContents);//path);
-          //}catch{//(URISyntaxException ex){
-          //  Log.d(TAG,"URI Syntax Exception");
-          //}
-
+          ReadFileAsyncTask task = new ReadFileAsyncTask();
+          task.execute(uri);
         }
         break;
     }
     super.onActivityResult(requestCode, resultCode, data);
   }
 
+  //read file AsyncTask
+  //3 params are <params, Progress, Result>
+  private class ReadFileAsyncTask extends AsyncTask<Uri, Void, String> {
 
-  public String readTextFile(Uri uri){
+    @Override
+    protected String doInBackground(Uri... uri) {
+      String response = "";
+      String storageState = Environment.getExternalStorageState();
+      Log.d(TAG,"uri: " + uri.toString());
+      if(storageState.equals(Environment.MEDIA_MOUNTED)){
+        //Log.d(TAG, "Media_Mounted");
+        File file = new File(Environment.getExternalStorageDirectory(),getFileName(uri[0]));//"wifiscan.csv");
+        //File file = new File(getExternalFilesDir(null),getFileName(uri[0]));//"wifiscan.csv");
+        Log.d(TAG, "file: " + file.toString());
+        //Log.d(TAG, "uri.getPath: " + getFileName(uri[0]));// uri.getPath());
+        try {
+          BufferedReader inputReader2 = new BufferedReader(
+                  new InputStreamReader(new FileInputStream(file)));// getFileName(uri.toString()))));// uri.getPath())));
+          String inputString2;
+          StringBuffer stringBuffer2 = new StringBuffer();
+          while ((inputString2 = inputReader2.readLine()) != null) {
+            stringBuffer2.append(inputString2 + "\n");
+            //Log.d(TAG, inputString2);
+          }
+          return stringBuffer2.toString();
 
-//    Log.d(TAG,"1:" + uri.getPath());    //storage/emulated/0/wifiscan.csv
-//    Log.d(TAG,"2:" + getFileName(uri)); //wifiscan.csv
-    String storageState = Environment.getExternalStorageState();
-    if(storageState.equals(Environment.MEDIA_MOUNTED)){
-      Log.d(TAG, "Media_Mounted");
-      File file = new File(getExternalFilesDir(null),"wifiscan.csv");
-      Log.d(TAG, "File: " + file.toString());
-      Log.d(TAG, "uri.getPath: " + uri.getPath());
-      try {
-        BufferedReader inputReader2 = new BufferedReader(
-                new InputStreamReader(new FileInputStream(uri.getPath())));
-        String inputString2;
-        StringBuffer stringBuffer2 = new StringBuffer();
-        while ((inputString2 = inputReader2.readLine()) != null) {
-          stringBuffer2.append(inputString2 + "\n");
-          Log.d(TAG, inputString2);
+          //mTextView.setText(stringBuffer2.toString());
+        }catch(FileNotFoundException ex){
+          Log.d(TAG,"Filenotfound");
+        }catch(IOException ex){
+          Log.d(TAG,"IOException");
         }
-        mTextView.setText(stringBuffer2.toString());
-      }catch(FileNotFoundException ex){
-        Log.d(TAG,"Filenotfound");
-      }catch(IOException ex){
-        Log.d(TAG,"IOException");
       }
+
+      return response;
     }
 
-    return "";
+    @Override
+    protected void onPostExecute(String result) {
+       mTextView.setText(result);
+    }
   }
 
   //Thanks to
@@ -213,12 +211,40 @@ public class MainActivity extends Activity{
     return result;
   }
 
-  //download mURL to mTextView
-  public void onButtonClick(View v){
-    mTextView.setText("");
-    Toast.makeText(getApplicationContext(), "Downloading...", Toast.LENGTH_SHORT).show();
-    DownloadAsyncTask task = new DownloadAsyncTask();
-    task.execute();
-  }
+
+
+
 }
 
+
+
+
+//  public String readTextFile(Uri uri){
+//
+////    Log.d(TAG,"1:" + uri.getPath());    //storage/emulated/0/wifiscan.csv
+////    Log.d(TAG,"2:" + getFileName(uri)); //wifiscan.csv
+//    String storageState = Environment.getExternalStorageState();
+//    if(storageState.equals(Environment.MEDIA_MOUNTED)){
+//      Log.d(TAG, "Media_Mounted");
+//      File file = new File(getExternalFilesDir(null),"wifiscan.csv");
+//      Log.d(TAG, "File: " + file.toString());
+//      Log.d(TAG, "uri.getPath: " + uri.getPath());
+//      try {
+//        BufferedReader inputReader2 = new BufferedReader(
+//                new InputStreamReader(new FileInputStream(uri.getPath())));
+//        String inputString2;
+//        StringBuffer stringBuffer2 = new StringBuffer();
+//        while ((inputString2 = inputReader2.readLine()) != null) {
+//          stringBuffer2.append(inputString2 + "\n");
+//          Log.d(TAG, inputString2);
+//        }
+//        mTextView.setText(stringBuffer2.toString());
+//      }catch(FileNotFoundException ex){
+//        Log.d(TAG,"Filenotfound");
+//      }catch(IOException ex){
+//        Log.d(TAG,"IOException");
+//      }
+//    }
+//
+//    return "";
+//  }
