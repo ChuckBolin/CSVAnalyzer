d2185e53657e60bed3f7960e9ca38b512d8d3d27
Chuck Bolin <cbolin@sc.rr.com>
2015-07-02 22:58:28 -0400

    Successful read of media file

--- src/main/java/com/mush4brain/csvanalyzer/MainActivity.java
+++ src/main/java/com/mush4brain/csvanalyzer/MainActivity.java
@@ -4,12 +4,18 @@ package com.mush4brain.csvanalyzer;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.NetworkOnMainThreadException;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +23,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -43,8 +52,21 @@ public class MainActivity extends Activity{
     actionBar.setTitle("CSV Analyzer");
     actionBar.setSubtitle(R.string.app_version);
     actionBar.setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+
+//    String storageState = Environment.getExternalStorageState();
+//    if(storageState.equals(Environment.MEDIA_MOUNTED)){
+//      Log.d(TAG, "Media_Mounted");
+//
+//
+//      File file = new File(getExternalFilesDir(null),
+//              "wifiscan.csv");
+//      Log.d(TAG, file.toString());
+//
+//    }
+
   }
 
+  //download URL AsyncTask
   private class DownloadAsyncTask extends AsyncTask<String, Void, String> {
 
     @Override
@@ -81,6 +103,117 @@ public class MainActivity extends Activity{
     }
   }
 
+  //read file AsyncTask
+  private class ReadFileAsyncTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... urls) {
+      String response = "";
+
+
+
+      return response;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+      //Toast.makeText(mContext, "onPostExecute", Toast.LENGTH_LONG).show();
+      mTextView.setText(result);
+    }
+  }
+
+  public void onButtonFindClick(View v){
+    mTextView.setText("");
+    Toast.makeText(getApplicationContext(), "Finding...", Toast.LENGTH_SHORT).show();
+
+    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+    intent.setType("*/*");
+    intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+    try{
+      startActivityForResult(
+              Intent.createChooser(intent, "Select a file to load"),
+              0);
+    }catch(ActivityNotFoundException ex){
+      Toast.makeText(this, "Install a file manager.",Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    switch(requestCode){
+      case 0:
+        if(resultCode == RESULT_OK){
+          Uri uri = data.getData();
+          //try {
+            String fileContents = readTextFile(uri);// getPath(this, uri.getPath());
+            mTextView.setText(fileContents);//path);
+          //}catch{//(URISyntaxException ex){
+          //  Log.d(TAG,"URI Syntax Exception");
+          //}
+
+        }
+        break;
+    }
+    super.onActivityResult(requestCode, resultCode, data);
+  }
+
+
+  public String readTextFile(Uri uri){
+
+//    Log.d(TAG,"1:" + uri.getPath());    //storage/emulated/0/wifiscan.csv
+//    Log.d(TAG,"2:" + getFileName(uri)); //wifiscan.csv
+    String storageState = Environment.getExternalStorageState();
+    if(storageState.equals(Environment.MEDIA_MOUNTED)){
+      Log.d(TAG, "Media_Mounted");
+      File file = new File(getExternalFilesDir(null),"wifiscan.csv");
+      Log.d(TAG, "File: " + file.toString());
+      Log.d(TAG, "uri.getPath: " + uri.getPath());
+      try {
+        BufferedReader inputReader2 = new BufferedReader(
+                new InputStreamReader(new FileInputStream(uri.getPath())));
+        String inputString2;
+        StringBuffer stringBuffer2 = new StringBuffer();
+        while ((inputString2 = inputReader2.readLine()) != null) {
+          stringBuffer2.append(inputString2 + "\n");
+          Log.d(TAG, inputString2);
+        }
+        mTextView.setText(stringBuffer2.toString());
+      }catch(FileNotFoundException ex){
+        Log.d(TAG,"Filenotfound");
+      }catch(IOException ex){
+        Log.d(TAG,"IOException");
+      }
+    }
+
+    return "";
+  }
+
+  //Thanks to
+  //http://stackoverflow.com/questions/5568874/how-to-extract-the-file-name-from-uri-returned-from-intent-action-get-content
+  public String getFileName(Uri uri) {
+    String result = null;
+    if (uri.getScheme().equals("content")) {
+      Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+      try {
+        if (cursor != null && cursor.moveToFirst()) {
+          result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+        }
+      } finally {
+        cursor.close();
+      }
+    }
+    if (result == null) {
+      result = uri.getPath();
+      int cut = result.lastIndexOf('/');
+      if (cut != -1) {
+        result = result.substring(cut + 1);
+      }
+    }
+    return result;
+  }
+
+  //download mURL to mTextView
   public void onButtonClick(View v){
     mTextView.setText("");
     Toast.makeText(getApplicationContext(), "Downloading...", Toast.LENGTH_SHORT).show();
